7a61ba6521fcdb0f7d5f414f4a97eefc64ad0ff7
Li Qiang <573139851@qq.com>
2025-03-22 09:27:02 +0800

    test

---
+++ src/main/java/com/exampleB/ServiceB/Controller/ServiceBController.java
@@ -0,0 +1,21 @@
+package com.exampleB.ServiceB.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("api/serviceB")
+public class ServiceBController {
+
+    @Autowired
+    private RestTemplate resetTemplate;
+
+    @GetMapping("callServiceA")
+    public ResponseEntity<String>callServiceA(){
+        return resetTemplate.getForEntity("http://localhost:8088/api/ServiceA/data", String.class);
+    }
+}
---
+++ src/test/java/com/exampleB/ServiceB/ServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleB.ServiceB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
